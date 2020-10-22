9625af535dbce88b3b71fde651c7444ced7b4f1d
Hasan Demirtaş <utsukushihito@outlook.com>
2020-10-22 14:42:41 +0300

    New development. (#25)

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 [![idea](https://www.elegantobjects.org/intellij-idea.svg)](https://www.jetbrains.com/idea/)
-[![rultor](https://www.rultor.com/b/yegor256/rultor)](https://www.rultor.com/p/portlek/mapentry)
 
 [![Build Status](https://travis-ci.com/portlek/mapentry.svg?branch=master)](https://travis-ci.com/portlek/mapentry)
 ![Maven Central](https://img.shields.io/maven-central/v/io.github.portlek/mapentry?label=version)
@@ -11,6 +10,6 @@
   <version>${version}</version>
 </dependency>
 ```
-```gradle
+```groovy
 implementation("io.github.portlek:mapentry:${version}")
 ```
--- src/test/java/io/github/portlek/mapentry/MapEntryTest.java
+++ src/test/java/io/github/portlek/mapentry/MapEntryTest.java
@@ -25,72 +25,71 @@
 
 package io.github.portlek.mapentry;
 
-import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
 import org.llorllale.cactoos.matchers.IsTrue;
 import org.llorllale.cactoos.matchers.Throws;
 
 final class MapEntryTest {
 
-    @Test
-    public void getKey() {
-        final String key = "hello";
-        final String value = "world";
-        MatcherAssert.assertThat(
-            "Can't get key in the map entry",
-            new MapEntry<>(key, value).getKey(),
-            new IsEqual<>(key)
-        );
-    }
+  @Test
+  public void getKey() {
+    final String key = "hello";
+    final String value = "world";
+    new Assertion<>(
+      "Can't get key in the map entry",
+      MapEntry.from(key, value).getKey(),
+      new IsEqual<>(key)
+    ).affirm();
+  }
 
-    @Test
-    public void getValue() {
-        final String value = "bar";
-        MatcherAssert.assertThat(
-            "Can't get value in the map entry",
-            new MapEntry<>("foo", value).getValue(),
-            new IsEqual<>(value)
-        );
-    }
+  @Test
+  public void getValue() {
+    final String value = "bar";
+    new Assertion<>(
+      "Can't get value in the map entry",
+      MapEntry.from("foo", value).getValue(),
+      new IsEqual<>(value)
+    ).affirm();
+  }
 
-    @Test
-    public void cantSetValue() {
-        MatcherAssert.assertThat(
-            "MapEntry class shouldn't be mutable!",
-            () ->
-                new MapEntry<>("elegant", "objects").setValue("test"),
-            new Throws<>(UnsupportedOperationException.class)
-        );
-    }
+  @Test
+  public void cantSetValue() {
+    new Assertion<>(
+      "MapEntry class shouldn't be mutable!",
+      () ->
+        MapEntry.from("elegant", "objects").setValue("test"),
+      new Throws<>(UnsupportedOperationException.class)
+    ).affirm();
+  }
 
-    @Test
-    public void equalsTo() {
-        final String key = "eo";
-        final String value = "book";
-        MatcherAssert.assertThat(
-            "MapEntries are not equals",
-            new MapEntry<>(key, value).equals(new MapEntry<>(key, value)),
-            new IsTrue()
-        );
-    }
+  @Test
+  public void equalsTo() {
+    final String key = "eo";
+    final String value = "book";
+    new Assertion<>(
+      "MapEntries are not equals",
+      MapEntry.from(key, value).equals(MapEntry.from(key, value)),
+      new IsTrue()
+    ).affirm();
+  }
 
-    @Test
-    public void compareHash() {
-        MatcherAssert.assertThat(
-            "the hash code are not equals",
-            new MapEntry<>("elegant", "objects").hashCode(),
-            new IsEqual<>(32_739_498)
-        );
-    }
+  @Test
+  public void compareHash() {
+    new Assertion<>(
+      "the hash code are not equals",
+      MapEntry.from("elegant", "objects").hashCode(),
+      new IsEqual<>(32_739_498)
+    ).affirm();
+  }
 
-    @Test
-    public void toStringMethod() {
-        MatcherAssert.assertThat(
-            "ToString method returns unexpected value",
-            new MapEntry<>("somekey", "somevalue").toString(),
-            new IsEqual<>("somekey=somevalue")
-        );
-    }
-
+  @Test
+  public void toStringMethod() {
+    new Assertion<>(
+      "ToString method returns unexpected value",
+      MapEntry.from("somekey", "somevalue").toString(),
+      new IsEqual<>("somekey=somevalue")
+    ).affirm();
+  }
 }
